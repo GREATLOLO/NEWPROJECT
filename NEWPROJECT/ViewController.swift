86c3bc21b465f01fef75d01c762ea6d92ccc67c7
Keqing (Kevin) Li <kli2020@208-Engineering-11.local>
2019-04-03 10:52:36 -0700

    Initial Commit

---
+++ NEWPROJECT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NEWPROJECT
+//
+//  Created by Keqing (Kevin) Li on 4/3/19.
+//  Copyright © 2019 Keqing (Kevin) Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
